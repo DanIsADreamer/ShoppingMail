4ee24fdeb4cac4ca6fe4ba0caffca145a4641946
DanIsADreamer <dansdream@163.com>
2019-06-17 23:17:52 +0800

    第一次提交

---
+++ ShoppingCarDemo/src/main/java/com/danisadreamer/ShoppingCarDemo/ShoppingCarDemoApplication.java
@@ -0,0 +1,16 @@
+package com.danisadreamer.ShoppingCarDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+@EnableAutoConfiguration
+
+public class ShoppingCarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCarDemoApplication.class, args);
+	}
+
+}
